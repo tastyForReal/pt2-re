3820da39d5ffdd7f5478cdb0c277c99a82b54a0b
tastyForReal <obviousblook4@gmail.com>
2026-04-01 16:44:09 +0800

    .

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 curl -fsSL https://sh.rustup.rs | sh -s -- -y --default-toolchain=stable --profile=minimal
 . "$HOME/.cargo/env" && rustup component add rustfmt clippy
 rm -rf "$HOME/.cargo/registry/*"
-. "$HOME/.cargo/env" && cargo build
+. "$HOME/.cargo/env" && cargo build --features "soundfont"
